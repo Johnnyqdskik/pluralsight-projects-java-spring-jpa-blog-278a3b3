76c8d2524dfb1908f0f7721b7cbd8038618d198a
Nikola Stanić <nibscompany@gmail.com>
2022-02-23 22:57:25 +0100

    Creating category entity and repository

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-public class BlogController {
+public class  BlogController {
 
     private PostRepository postRepository;
 
--- src/main/java/com/pluralsight/blog/model/Category.java
+++ src/main/java/com/pluralsight/blog/model/Category.java
@@ -5,13 +5,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+@Entity
 public class Category {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
+    @OneToMany
+    List<Post> posts;
+
+
     public Category() {
         super();
+        posts = new ArrayList<>();
     }
 
     public Long getId() {
@@ -26,11 +35,12 @@ public class Category {
         this.name = name;
     }
 
+
     public List<Post> getPosts() {
-        return null;
+        return posts;
     }
 
     public void addPost(Post post) {
-        return;
+        posts.add(post);
     }
 }
