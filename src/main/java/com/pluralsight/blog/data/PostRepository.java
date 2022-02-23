4605fda45a92b1cc15ed3cfda82427cab802146d
Nikola Stanić <nibscompany@gmail.com>
2022-02-23 18:51:05 +0100

    Moved file data.sql

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -4,4 +4,4 @@ package com.pluralsight.blog.data;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {} 
+public interface PostRepository extends JpaRepository<Post, Long> {}
